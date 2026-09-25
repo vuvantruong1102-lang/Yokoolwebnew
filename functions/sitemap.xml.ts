175d6e93e9f6301c67efd6e025794248ade5a8e3
vuvantruong1102-lang <vuvantruong.1102@gmail.com>
2026-09-25 15:28:44 +0700

    Add files via upload

--- functions/sitemap.xml.ts
+++ functions/sitemap.xml.ts
@@ -44,6 +44,8 @@ export const onRequestGet: PagesFunction<Env> = async (context) => {
       { loc: `${baseUrl}/products/ol212.html`, priority: '0.9', changefreq: 'weekly' },
       { loc: `${baseUrl}/products/atlas20.html`, priority: '0.9', changefreq: 'weekly' },
       { loc: `${baseUrl}/products/nano.html`, priority: '0.9', changefreq: 'weekly' },
+      { loc: `${baseUrl}/products/nomad25.html`, priority: '0.9', changefreq: 'weekly' },
+      { loc: `${baseUrl}/products/atlas70.html`, priority: '0.9', changefreq: 'weekly' },
       { loc: `${baseUrl}/b2b.html`, priority: '0.7', changefreq: 'monthly' },
       { loc: `${baseUrl}/lien-he.html`, priority: '0.5', changefreq: 'monthly' },
       { loc: `${baseUrl}/bao-hanh.html`, priority: '0.5', changefreq: 'monthly' },
